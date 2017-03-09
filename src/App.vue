553d0d696d2433fb591bec4d595283ebf1101d70
Jon Short <JonShort@users.noreply.github.com>
2017-03-09 22:21:37 +0000

    added the old code back in

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,79 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  <div id="app" v-bind:style="{backgroundColor: color}">
+    <section :class="$style.text">
+      <p><span :class="$style.quote">{{current}}</span> {{subject}}</p>
+    </section>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-};
+  const SubjectName = 'Jonathan Gill';
+
+  const QuoteList = [
+    'Run of the mill',
+    'Over the hill',
+    'Ate a dodgy pill',
+    'Going in for the kill',
+    'Writing his will',
+    'Sharpening his drill',
+    'Somehow he is still',
+    'All on his bill',
+    'At the shopping till',
+  ];
+
+  const currentQuote = QuoteList[Math.floor(Math.random() * QuoteList.length)];
+
+  const ColorList = [
+    '#e7040f',
+    '#ff6300',
+    '#ffb700',
+    '#5e2ca5',
+    '#d5008f',
+    '#ff41b4',
+    '#137752',
+    '#001b44',
+    '#357edd',
+  ];
+
+  const currentColor = ColorList[Math.floor(Math.random() * ColorList.length)];
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        color: currentColor,
+        current: currentQuote,
+        subject: SubjectName,
+      };
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html {
+    font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+  }
+  html, body, #app {
+    align-items: center;
+    display: flex;
+    height: 100%;
+    justify-content: center;
+    width: 100%;
+  }
+  body {
+    margin: 0;
+  }
+  * {
+    box-sizing: border-box;
+  }
+</style>
+
+<style module>
+  .text {
+    font-size: 5vw;
+  }
+
+  .quote {
+    color: #FFFFFF;
+  }
 </style>
