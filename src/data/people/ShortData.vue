d16002edd3cf8e17b20cf6800e5dee1a5825ce95
Jon Short <JonShort@users.noreply.github.com>
2017-04-02 11:43:56 +0100

    added myself

---
+++ src/data/people/ShortData.vue
@@ -0,0 +1,17 @@
+<script>
+  export const SubjectName = 'Jon Short';
+
+  export const QuoteList = [
+    'Cannot be bought',
+    'Easily taught',
+    'On the tennis court',
+    'Likes to drink port',
+    'Never been caught',
+    'Always distraught',
+    'Building a fort',
+    'Able to teleport',
+    'At the resort',
+  ];
+
+  export const Link = 'https://twitter.com/altshort';
+</script>
